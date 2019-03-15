91ebe233b3b40dff3a85daa980f146576cb86082
rosesharondi@icloud.com <rosesharondi@icloud.com>
2019-03-14 19:41:44 -0500

    Add project files.

---
+++ Yahtzee_Sharon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yahtzee_Sharon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
